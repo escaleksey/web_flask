4f95283b9e4ad8e1ebb5b1c0ba806036759eda9a
Aleksey Kushnarev <scanavik2005@gmail.com>
2022-03-09 19:47:06 +0500

    task5

--- index.py
+++ index.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
+
+
+class LoginForm(FlaskForm):
+    username1 = StringField('ID Астронавта', validators=[DataRequired()])
+    password1 = PasswordField('Пароль Астронавта', validators=[DataRequired()])
+    username2 = StringField('ID Капитана', validators=[DataRequired()])
+    password2 = PasswordField('Пароль Капитана', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
 
 @app.route('/')
 @app.route('/index')
@@ -24,6 +37,13 @@ def answer():
     param['ready'] = 'True'
 
     return render_template('answer.html', **param)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/index')
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
